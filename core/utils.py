f9491306723278211185dbc7f8ae5f6cbf5908ed
Emmanuel Jakobowicz <39994746+emjako@users.noreply.github.com>
2025-03-13 13:24:02 +0100

    Update utils.py - add docstrings

--- core/utils.py
+++ core/utils.py
@@ -2,5 +2,15 @@ from config.logger import get_logger
 logger = get_logger(__name__)
 
 def addition(a, b):
+    """
+    Additionne deux nombres.
+
+    Args:
+        a (int | float): premier opérande
+        b (int | float): second opérande
+
+    Returns:
+        int | float: résultat de l'addition
+    """
     logger.debug(f"Addition de {a} + {b}")
     return a + b
